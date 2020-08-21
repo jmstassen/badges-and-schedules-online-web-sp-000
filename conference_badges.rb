a19259969f6c77ae83dbecfdda29990d201f7780
John Stassen <john.stassen@gmail.com>
2020-08-21 04:37:43 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,4 +11,9 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees)
-  
+  room_assignments = []
+  attendees.each do |attendee,index|
+    room_assignments << "Hello, #{attendee}! You'll be assigned to room " + (index+1)
+  end
+  return room_assignments
+end
