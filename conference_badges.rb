cc5e490f2a7d94599095b6894fd3c7456372b7f1
John Stassen <john.stassen@gmail.com>
2020-08-21 04:32:48 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,11 @@
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(attendees)
+  badges = []
+  attendees.each do |attendee|
+    badged << "Hello, my name is #{attendee}."
+  end
+  return badges
+end
